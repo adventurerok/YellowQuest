e2eb5c92bc69aa8d5ad11bccd72c34abb3fe0c14
Paul Durbaba <paul.durbaba@btinternet.com>
2014-08-16 10:29:06 +0100

    Added the google play libraries

--- YellowQuest/src/com/ithinkrok/yellowquest/MainActivity.java
+++ YellowQuest/src/com/ithinkrok/yellowquest/MainActivity.java
@@ -23,12 +23,10 @@ public class MainActivity extends BaseGameActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
-		
-		super.onCreate(savedInstanceState);
-
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		super.onCreate(savedInstanceState);
 
 		view = new CanvasSurfaceView(this);
 		setContentView(view);
